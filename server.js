8b57dabba433c68590bcf9a27f9ad1eb7c41f1e7
skiryuk <skiryuk@gmail.com>
2018-06-14 01:21:13 +0500

    add express

--- server.js
+++ server.js
@@ -1,17 +1,6 @@
 const express = require('express');
 const app = express();
-const forceSSL = function() {
-  return function (req, res, next) {
-    if (req.headers['x-forwarded-proto'] !== 'https') {
-      return res.redirect(
-        ['https://', req.get('Host'), req.url].join('')
-      );
-    }
-    next();
-  }
-}
 app.use(express.static(__dirname + '/dist'));
-app.use(forceSSL());
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
