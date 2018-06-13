c6cb3886e95b2f1699c30d626b55d9d97e235594
skiryuk <skiryuk@gmail.com>
2018-06-14 01:57:26 +0500

    add express

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static('/email-viewer/dist'));
+app.use(express.static(__dirname + '/dist'));
 
 app.get('/*', function(req,res) {
-  res.sendFile('/email-viewer/dist/index.html');
+  res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
 app.listen(process.env.PORT || 8080);
