fecf0324bdb0590b8c81346fdd4e5b96fc68726d
skiryuk <skiryuk@gmail.com>
2018-06-14 01:37:23 +0500

    add express

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const path = require('express');
+const path = require('path');
 const app = express();
 app.use(express.static(__dirname + '/dist'));
 app.get('/*', function(req, res) {
