cff01497abd97c2a600d249becfbe3e381cacd21
skiryuk <skiryuk@gmail.com>
2018-06-14 01:48:06 +0500

    add express

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static('./dist'));
 
 app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname+'/dist/index.html'));
+  res.sendFile('./dist/index.html');
 });
 app.listen(process.env.PORT || 8080);
