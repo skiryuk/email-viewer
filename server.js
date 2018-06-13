4af669daf7db14e838aaa310f58c69cbfa439b0a
skiryuk <skiryuk@gmail.com>
2018-06-14 01:09:15 +0500

    add express

--- server.js
+++ server.js
@@ -1,17 +1,4 @@
 const express = require('express');
-const path = require('path');
 const app = express();
-
-const port = process.env.PORT || '8080';
-app.set('port', port);
-
-app.use(express.static(__dirname + '/dist'));
-
-app.get('/[^\.]+$', function(req, res) {
-  res.set('Content-Type', 'text/html')
-    .sendFile(path.join(__dirname, '/dist/index.html'));
-});
-
-app.listen(app.get('port'), function() {
-  console.log("Listening on port " + app.get('port'));
-})
+app.use(express.static(__dirname + './dist'));
+app.listen(process.env.PORT || 8080);
